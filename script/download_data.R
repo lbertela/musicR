16d83c4b269393d69701db7fa89d95d4c7fdb204
lbertela <ludovic.bertela@hotmail.com>
2024-05-02 23:00:58 +0200

    Structure script

--- script/download_data.R
+++ script/download_data.R
@@ -1,21 +1,25 @@
 rm(list=ls())
 Sys.setenv(LANG = "en")
 
+# Set Paths and files
 path_onedrive <- "C:/Users/ludov/OneDrive/Inventaire Vinyle et CD/"
-path_jpg <- "C:/Users/ludov/Saved Games/Documents/music_center/www"
-path_xlsx <- "C:/Users/ludov/Saved Games/Documents/music_center"
+path_jpg <- "C:/Users/ludov/Saved Games/Documents/GitHub/music_center/app/www"
+path_xlsx <- "C:/Users/ludov/Saved Games/Documents/GitHub/music_center/app"
 
-files <- list.files(path = "C:/Users/ludov/OneDrive/Inventaire Vinyle et CD/")
+files <- list.files(path = path_onedrive)
 
+# Identify .jpg and .xlsx files
 jpg_files <- files[grepl(".jpg", files)]
 xlsx_file <- files[grepl(".xlsx", files)]
 
+# Save each .jpg file
 for(file in jpg_files) {
      file.copy(from = file.path(path_onedrive, file),
                to = path_jpg,
                overwrite = TRUE)
 }
 
+# Save the .xlsx database
 file.copy(from = file.path(path_onedrive, xlsx_file),
           to = path_xlsx,
           overwrite = TRUE)
