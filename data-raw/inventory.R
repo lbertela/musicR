334b82158ccc2c60f1bdc84f2158f74bad3a7274
lbertela <ludovic.bertela@hotmail.com>
2024-05-31 00:11:41 +0200

    Remove app.R and add filtering of the inventory directly in inventory.R

--- data-raw/inventory.R
+++ data-raw/inventory.R
@@ -4,7 +4,7 @@ Sys.setenv(LANG = "en")
 
 # Set Paths and files
 path_onedrive <- "C:/Users/ludov/OneDrive/Inventaire Vinyle et CD/"
-path_www <- "app/www"
+path_www <- "inst/app/www"
 
 files <- list.files(path = path_onedrive)
 
@@ -12,14 +12,27 @@ files <- list.files(path = path_onedrive)
 jpg_files <- files[grepl(".jpg", files)]
 xlsx_file <- files[grepl(".xlsx", files)]
 
-# Save each .jpg file
+# Save each .jpg file into "www"
 for(file in jpg_files) {
      file.copy(from = file.path(path_onedrive, file),
                to = path_www,
                overwrite = TRUE)
 }
 
-# Save the .xlsx database
-inventory <- openxlsx::read.xlsx(file.path(path_onedrive, xlsx_file))
+# Load inventory
+inventory <- openxlsx::read.xlsx(file.path(path_onedrive, xlsx_file), 
+                                 startRow = 2, rowNames = FALSE)
 
+names(inventory) <- c("group", "album", "artist", "year", "genre", 
+                      "price", "type", "cover", "location", "link")
+
+# Prepare inventory
+inventory <- inventory %>% 
+     mutate(price = case_when(is.na(price) ~ 0,
+                              price == "-" ~ 0,
+                              TRUE ~ as.numeric(price))) %>% 
+     mutate(cover = gsub(".jpg", "", cover)) %>% 
+     relocate(group, cover)
+
+# Save the inventory
 usethis::use_data(inventory, overwrite = TRUE)
