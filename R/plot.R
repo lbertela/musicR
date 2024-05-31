12c63385333a8e03ff442acd11fb23337ad060cf
lbertela <ludovic.bertela@hotmail.com>
2024-06-01 00:51:17 +0200

    Create and implement plot functions

---
+++ R/plot.R
@@ -0,0 +1,68 @@
+#' Plot the frequency of a variable on horizontal bars
+#'
+#' @param data data.frame to use
+#' @param variable variable we are interested in
+#'
+#' @return plot of type ggplot
+#' @export
+#'
+#' @examples
+plot_freq_bar <- function(data, variable) {
+     
+     dt_plot <- data.frame(table(data[, variable])) %>% 
+          rename(group = Var1) %>% 
+          rename(value = Freq) %>%
+          mutate(group = forcats::fct_reorder(group, value))
+     
+     p <- ggplot(dt_plot, aes(x=group, y=value)) +
+          geom_bar(stat="identity", fill="#f68060", alpha=.7, width=.3) +
+          theme_minimal() +
+          coord_flip() +
+          xlab("") +
+          ylab("") + 
+          theme(axis.text=element_text(color="white"),
+                axis.ticks.x=element_blank(),
+                axis.ticks.y=element_blank(),
+                panel.grid = element_line(color = "darkgrey",
+                                          linewidth = 0.1),
+                panel.border = element_rect(fill = "transparent",
+                                            color = "darkgrey")
+          )
+     
+     return(p)
+     
+}
+
+#' Plot the frequency of a variable on a histogram
+#'
+#' @param data data.frame to use
+#' @param variable variable we are interested in
+#'
+#' @return plot of type ggplot
+#' @export
+#'
+#' @examples
+plot_freq_hist <- function(data, variable) {
+     
+     dt_plot <- data.frame(var = data[, variable])
+     
+     p <- ggplot(dt_plot, aes(x=var)) +
+          geom_histogram(binwidth=3, colour="black", fill="#f68060", 
+                         alpha=0.7, linewidth=0.25) +
+          theme_minimal() +
+          xlab(variable) +
+          ylab("Albums") + 
+          theme(axis.text=element_text(color="white"),
+                axis.title.x = element_text(colour = "white"),
+                axis.title.y = element_text(colour = "white", vjust = 3),
+                axis.ticks.x=element_blank(),
+                axis.ticks.y=element_blank(),
+                panel.grid = element_line(color = "darkgrey",
+                                          linewidth = 0.1),
+                panel.border = element_rect(fill = "transparent",
+                                            color = "darkgrey")
+          )
+     
+     return(p)
+     
+}
--- inst/app/server.R
+++ inst/app/server.R
@@ -77,105 +77,19 @@ server <- function(input, output, session) {
      )
      
      output$histo1 <- renderPlot({
-          
-          dt_plot <- as.data.frame(table(inventory$type)) %>% 
-               dplyr::rename(group = Var1) %>% 
-               dplyr::rename(value = Freq) %>%
-               mutate(group = forcats::fct_reorder(group, value))
-          
-          p <- ggplot(dt_plot, aes(x=group, y=value)) +
-               geom_bar(stat="identity", fill="#f68060", alpha=.7, width=.3) +
-               theme_minimal() +
-               coord_flip() +
-               xlab("") +
-               ylab("") + 
-               theme(axis.text=element_text(color="white"),
-                     axis.ticks.x=element_blank(),
-                     axis.ticks.y=element_blank(),
-                     panel.grid = element_line(color = "darkgrey",
-                                               linewidth = 0.1),
-                     panel.border = element_rect(fill = "transparent",
-                                                 color = "darkgrey")
-               )
-          
-          p
-          
+          plot_freq_bar(data = inventory, variable = "type")
      }, width = 1000, height = 480, res = 150, bg="transparent")
      
      output$histo2 <- renderPlot({
-          
-          dt_plot <- data.frame(price = inventory$price)
-          
-          p <- ggplot(dt_plot, aes(x=price)) +
-               geom_histogram(binwidth=5, colour="black", fill="#f68060", 
-                              alpha=0.7, linewidth=0.25) +
-               theme_minimal() +
-               xlab("Prix") +
-               ylab("Albums") + 
-               theme(axis.text=element_text(color="white"),
-                     axis.title.x = element_text(colour = "white"),
-                     axis.title.y = element_text(colour = "white", vjust = 3),
-                     axis.ticks.x=element_blank(),
-                     axis.ticks.y=element_blank(),
-                     panel.grid = element_line(color = "darkgrey",
-                                               linewidth = 0.1),
-                     panel.border = element_rect(fill = "transparent",
-                                                 color = "darkgrey")
-               )
-          
-          p
-          
+          plot_freq_hist(data = inventory, variable = "price")
      }, width = 500, height = 480, res = 150, bg="transparent")
      
      output$histo3 <- renderPlot({
-          
-          dt_plot <- as.data.frame(table(inventory$genre)) %>% 
-               dplyr::rename(group = Var1) %>% 
-               dplyr::rename(value = Freq) %>%
-               mutate(group = forcats::fct_reorder(group, value))
-          
-          
-          p <- ggplot(dt_plot, aes(x=group, y=value)) +
-               geom_bar(stat="identity", fill="#f68060", alpha=.7, width=.3) +
-               theme_minimal() +
-               coord_flip() +
-               xlab("") +
-               ylab("") + 
-               theme(axis.text=element_text(color="white"),
-                     axis.ticks.x=element_blank(),
-                     axis.ticks.y=element_blank(),
-                     panel.grid = element_line(color = "darkgrey",
-                                               linewidth = 0.1),
-                     panel.border = element_rect(fill = "transparent",
-                                                 color = "darkgrey")
-               )
-          
-          p
-          
+          plot_freq_bar(data = inventory, variable = "genre")
      }, width = 1000, height = 480, res = 150, bg="transparent")
      
      output$histo4 <- renderPlot({
-          
-          dt_plot <- data.frame(year = inventory$year)
-          
-          p <- ggplot(dt_plot, aes(x=year)) +
-               geom_histogram(binwidth=3, colour="black", fill="#f68060", 
-                              alpha=0.7, linewidth=0.25) +
-               theme_minimal() +
-               xlab("Année") +
-               ylab("Albums") + 
-               theme(axis.text=element_text(color="white"),
-                     axis.title.x = element_text(colour = "white"),
-                     axis.title.y = element_text(colour = "white", vjust = 3),
-                     axis.ticks.x=element_blank(),
-                     axis.ticks.y=element_blank(),
-                     panel.grid = element_line(color = "darkgrey",
-                                               linewidth = 0.1),
-                     panel.border = element_rect(fill = "transparent",
-                                                 color = "darkgrey")
-               )
-          
-          p
-          
+          plot_freq_hist(data = inventory, variable = "year")
      }, width = 500, height = 480, res = 150, bg="transparent")
+     
 }
