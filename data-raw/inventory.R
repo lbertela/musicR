9aaf55d39dccabed7b95ef98c4ed5f043076f078
lbertela <ludovic.bertela@hotmail.com>
2025-01-11 00:52:04 +0100

    Fix inventory.R

--- data-raw/inventory.R
+++ data-raw/inventory.R
@@ -1,10 +1,7 @@
 ## code to prepare `inventory` dataset goes here
-rm(list=ls())
-Sys.setenv(LANG = "en")
-
 # Set Paths and files
 path_onedrive <- "C:/Users/ludov/OneDrive/Inventaire Vinyle et CD/"
-path_www <- "inst/www"
+path_www <- "inst/app/www"
 
 files <- list.files(path = path_onedrive)
 
