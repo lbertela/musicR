aefd3b9f7ddb07b5218dd8ba19bebfda5bac085c
lbertela <ludovic.bertela@hotmail.com>
2025-01-11 11:52:19 +0100

    update data

--- data-raw/language.R
+++ data-raw/language.R
@@ -1,8 +1,8 @@
-devtools::load_all()
+## code to prepare `language` dataset goes here
 
 language <- data.frame(EN = names(inventory),
                        FR = c("groupes", "couverture", "albums", "artistes", "années", 
-                              "genres", "prix", "supports", "propiété", "lien"))
+                              "genres", "prix", "supports", "propiété"))
 
 # Save the language data
 usethis::use_data(language, overwrite = TRUE)
