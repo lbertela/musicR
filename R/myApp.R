0c2dcdb80b5f28ddf0d540690f2d3f080be4d209
lbertela <ludovic.bertela@hotmail.com>
2025-01-11 10:47:14 +0100

    remove commented section

--- R/myApp.R
+++ R/myApp.R
@@ -2,18 +2,6 @@
 #'
 #' @description This function launches the Shiny app included in the `musicr`.
 #' @export
-# myApp <- function() {
-#      # Locate the app directory
-#      app_dir <- file.path(here::here(), "inst/app")
-#      
-#      # Source the ui and server scripts
-#      source(file.path(app_dir, "ui.R"), local = TRUE)
-#      source(file.path(app_dir, "server.R"), local = TRUE)
-#      
-#      # Run the Shiny app
-#      shinyApp(ui = ui, server = server)
-# }
-
 myApp <- function() {
      # Locate the app directory relative to the package
      app_dir <- system.file("app", package = "musicr")
