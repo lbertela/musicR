6f0c26238961dba43b4cbb7ca049f7d9e4499b9f
lbertela <ludovic.bertela@hotmail.com>
2026-01-15 23:37:44 +0100

    script to deploy app

---
+++ scripts/deploy_app.R
@@ -0,0 +1,5 @@
+remove.packages("musicr")
+remotes::install_github("https://github.com/lbertela/musicR.git")
+musicr::myApp()
+
+# With the Rstudio Viewer, publish the app under the name you want, like "music_center_ali"
